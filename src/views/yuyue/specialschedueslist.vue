0e83f7ecfae1000797405c53424ceee97313a0a3
zdlian <zdlian@hotmail.com>
2018-02-01 20:06:10 +0800

    排班

--- src/views/yuyue/specialschedueslist.vue
+++ src/views/yuyue/specialschedueslist.vue
@@ -9,15 +9,32 @@
                     <Icon type="compose"></Icon>
                     排班详情
                 </p>
-                <Tabs :animated="false"   @on-click="setCurrent">
-                    <TabPane label="2018-01-31" name="2018-01-31"></TabPane>
-                    <TabPane label="2018-02-01" name="2018-02-01"></TabPane>
-                    <TabPane label="2018-02-02" name="2018-02-02"></TabPane>
-                    <TabPane label="2018-02-03" name="2018-02-03"></TabPane>
-                    <TabPane label="2018-02-04" name="2018-02-04"></TabPane>
+                <Tabs :animated="false"  :value='CurrentDate'   @on-click="setCurrent"> 
+                    <TabPane   v-for="option in QueryDate" :label="option.Day" :name="option.Day"></TabPane>
                 </Tabs>
-                <Table :v-if="showTable==true" :columns="shopping_col" :data="shopping_data"></Table>
-                <p style="margin-top:20px;">
+                <Table v-if="showTable==true" :columns="shopping_col" :data="shopping_data">
+                    
+                </Table>
+                <Form  v-if="modalList==true" :model="initCustom" :rules="initCustom" :label-width="180" style="margin-top:20px;">
+                    <FormItem label="选择所属门店" prop="StoreId">
+                        <Select v-model="initCustom.StoreId"  style="width: 500px"  placeholder="">
+                             <Option v-for="item in storelist" :value="item.Id" :key="item.Id">{{ item.Name }}</Option>
+                         </Select>
+                     </FormItem>
+                     <FormItem label="开始时间" prop="StartTime">
+                         <TimePicker  format="HH:mm"  v-model="initCustom.StartTime" style="width: 500px"></TimePicker>
+                     </FormItem>
+                      <FormItem label="结束时间" prop="EndTime">
+                         <TimePicker  format="HH:mm"   v-model="initCustom.EndTime" style="width: 500px"></TimePicker>
+                     </FormItem>
+                    <div style="margin-top:20px;margin-left:200px">
+                        <Button type="primary"   @click="generateTime" :loading="loading2">生成排班时间</Button>
+                    </div>
+                </Form>
+                <p style="margin-top:20px;"  v-if="showTable==true" >
+                     <Button type="default" @click="addOneLine"><Icon type="plus" />&nbsp;&nbsp;新增时间段</Button>  <Checkbox v-model="Single">是否成为模板</Checkbox>
+                </p>
+                <p style="margin-top:20px;" v-if="showTable==true">
                      <Button type="primary" @click="saveCurrent">保存当前</Button>
                 </p>
             </Card>
@@ -40,107 +57,308 @@ export default {
         return {
             showInfo: false,
             showTable:true,
+            modalList:false,
+            loading2:false,
             shopping_col: [
                 {
                     title: '门店',
-                    key: 'name',
-                    align: 'center'
+                    key: 'StoreId',
+                    align: 'center',
+                    render:(h,p)=>{
+                            let stolist=this.storelist;
+                            return h('Select', {
+                                props:{
+                                     value: p.row.StoreId,
+                                     model:'StoreId'
+                                },
+                                on:{
+                                    'on-change':(event) => {
+                                        this.shopping_data[p.index].StoreId = event.target.value;
+                                    }
+                                }
+                            },[
+                                stolist.map((t,i)=>{
+                                    return h('Option', {
+                                        props: {
+                                            value: t.Id,
+                                            key:t.Id
+                                        }
+                                    },t.Name)
+                                })
+                            ])
+                    }
                 },
                 {
                     title: '时间',
-                    key: 'time',
-                    align: 'center'
+                    key: 'SchedueDate',
+                    align: 'center',
+                    render:(h,p)=>{
+                        return h('Input',{
+                            props:{
+                                     value: p.row.SchedueDate
+                            },
+                            style: {
+　　　　　　　　　　              width: '80px'
+　　　　　　　　              },
+                            on:{
+                               'on-blur':(event) => {
+                                    this.shopping_data[p.index].SchedueDate = event.target.value;
+                                 }
+                            }
+                        })
+
+                    }
                 },
                 {
                     title: '预约费用',
-                    key: 'price',
-                    align: 'center'
+                    key: 'OrderPrice',
+                    align: 'center',
+                    render:(h,p)=>{
+                        return h('Input',{
+                            props:{
+                                value: p.row.OrderPrice
+                            },
+                            style: {
+　　　　　　　　　　              width: '80px'
+　　　　　　　　              },
+                            on:{
+                               'on-blur':(event) => {
+                                    this.shopping_data[p.index].OrderPrice = event.target.value;
+                                 }
+                            }
+                        })
+                    }
                 },
                 {
                     title: '到店另付费用',
-                    key: 'price',
-                    align: 'center'
+                    key: 'AnotherPrice',
+                    align: 'center',
+                    render:(h,p)=>{
+                        return h('Input',{
+                            props:{
+                                  value: p.row.AnotherPrice
+                                },
+                            style: {
+　　　　　　　　　　              width: '80px'
+　　　　　　　　              },
+                            on:{
+                               'on-blur':(event) => {
+                                    this.shopping_data[p.index].AnotherPrice = event.target.value;
+                                 }
+                            }
+                        })
+                    }
                 },
                 {
                     title: '名额',
-                    key: 'price',
-                    align: 'center'
-                }
-                ,
+                    key: 'Count',
+                    align: 'center',
+                    render:(h,p)=>{
+                        return h('Input',{
+                            props:{
+                                value: p.row.Count
+                            },
+                            style: {
+　　　　　　　　　　              width: '80px'
+　　　　　　　　              },
+                            on:{
+                               'on-blur':(event) => {
+                                    this.shopping_data[p.index].Count = event.target.value;
+                                }
+                            }
+                        })
+                    }
+                },
                 {
                     title: '操作',
                     key: 'price',
-                    align: 'center'
-                }
-                ,
+                    align: 'center',
+                    render: (h, params)=>{
+                        return h('div', [
+                            h("Button",{
+                                style: {margin: "0 5px"},
+                                props: {
+                                    type: "error",
+                                    placement: "top",
+                                    size: "small"
+                                },
+                                on: {
+                                    click: () => {
+                                        let thisTableData=this.shopping_data;
+                                        thisTableData.splice(params.index,1);
+                                        thisTableData.$Message.success('删除成功');
+                                    }
+                                }
+                            },"删除")
+                        ]);
+                    }
+                },
                 {
                     title: '是否临时',
-                    key: 'price',
-                    align: 'center'
+                    key: 'IsTemporary',
+                    align: 'center',
+                    render:(h,p)=>{
+                            let templist= this.IsTemp;
+                            return h('Select', {
+                                props:{
+                                    value: (p.row.IsTemporary?1:0)
+                                },
+                                style:{
+                                    width:'80px'
+                                }
+                            },[
+                                templist.map((t,i)=>{
+                                    return h('Option', {
+                                        props: {
+                                            value: t.Id,
+                                            key:t.Id
+                                        }
+                                    },t.Name)
+                                })
+                            ])
+                    }
                 },
                 {
                     title: '是否模板',
-                    key: 'price',
-                    align: 'center'
+                    key: 'IsTemplate',
+                    align: 'center',
+                    render:(h,p)=>{
+                        return h('Input',{
+                            props:{
+                                     value: p.row.IsTemplate?"是":"否",
+                                     disabled :'disabled'
+                                },
+                            style: {
+　　　　　　　　　　              width: '80px'
+　　　　　　　　              }
+                        })
+                    }
                 }
             ],
             shopping_data: [],
             formCustom:{
-                    Id:this.$route.params.id,
-                    CurrentDate:'2018-01-31'
+                Id:this.$route.params.id
             },
-            geturl:'admin/Special/Order/Detail'
+            initCustom:{
+                StoreId:'',
+                StartTime:'8:30',
+                EndTime:'17:00'
+            },
+            geturl:'admin/Special/Schedues/Record',
+            storelist:[],
+            IsTemp:[{Id:1,Name:'是'},{Id:0,Name:'否'}],
+            QueryDate:[],
+            CurrentDate:'',
+            Single:true
         };
     },
     methods: {
         init () {
             let vm=this;
-            let name = '';
-            let time = '';
-            let price = '';
-            switch (this.formCustom.Id) {
-                case 3: 
-                    name = '《vue.js实战》'; time = '20ry7年11月12日 13：33：24'; price = '79';
-                 break;
-                case 4: name = '面包'; time = '2017年11月5日 19：13：24'; price = '10'; break;
-                case 5: name = '咖啡'; time = '2017年11月8日 10：39：24'; price = '57'; break;
-                case 6: name = '超级豪华土豪金牙签'; time = '2017年11月9日 11：45：24'; price = '200'; break;
-            }
-            let shoppingInfor = {
-                name: '',
-                time: time,
-                price: price
-            };
-            this.shopping_data = [shoppingInfor];
-
-            vm.formCustom.CurrentDate='2018-01-31';
-            //load message
+            //是否成为模板
+            vm.single=false;
+        },
+        setCurrent(value){
+            let vm=this;
+            vm.CurrentDate=value;
+            this.getDataInfo();
+        },
+        getDataInfo(){
+            let vm=this;
             if(this.formCustom.Id&&this.formCustom.Id!=0){
-                Util.post(this.geturl,{Id:this.formCustom.Id},vm,function(res,data){
+                Util.post(this.geturl,{SId:this.formCustom.Id,Day:this.CurrentDate},vm,function(res,data){
                     if(res==='1'){
-                         vm.formCustom=data.data;
-                    }else{
-                         this.$Message.error('获取订单信息出错了');
+                         if(data.totalCount>0){
+                            vm.showTable=true;
+                            vm.modalList=false;
+                            vm.shopping_data = data.data;         
+                        }else{
+                            vm.modalList=true;
+                            vm.showTable=false;
+                        }
                     }
                });
             }
         },
-        setCurrent(value){
-            let vm=this;
-            vm.formCustom.CurrentDate=value;
-        },
         saveCurrent() {
             let vm=this;
+            var data2=this.shopping_data;
+            data2.map((t,i)=>{
+                t.SchedueDay=this.CurrentDate;
+            })
+            
+            Util.post("admin/Special/Schedues/Save",{SId:this.formCustom.Id,Day:this.CurrentDate,Single:this.single,SchedueString:JSON.stringify(data2)},vm,function (res,data) {
+                if(res==='1'){
+                    vm.$Message.success('保存成功');
+                    vm.getDataInfo();
+                }
+            })
+        },
+        addOneLine() {
+            let vm=this;
             vm.shopping_data.push( {
-                name: "222",
-                time: "sss",
-                price: "734"
-            });
-            console.log(vm.formCustom.CurrentDate);
+                 StoreId: 0,
+                 SchedueDate: '',
+                 OrderPrice: '',
+                 AnotherPrice:'',
+                 Count:1,
+                 IsTemplate:false,
+                 IsTemporary:true
+            }); 
+        },
+        generateTime(){
+            let vm=this;
+            //基础信息
+            let storeId = '';
+            let startTime= '';
+            let endTime='';
+            storeId = vm.initCustom.StoreId;
+            startTime= '2018-01-02 08:30'; //vm.initCustom.StartTime;
+            endTime= '2018-01-02 17:00'; //vm.initCustom.EndTime;
+            //获取时间段
+            Util.post("admin/Special/Schedues/GetDate",{SId:this.formCustom.Id,StoreId:storeId,Day:this.CurrentDate,Start:startTime,End:endTime},vm,function (res,data) {
+                 if(res==='1'){
+                         if(data.totalCount>0){
+                            vm.showTable=true;
+                            vm.modalList=false;
+                            vm.shopping_data = data.data;         
+                        }else{
+                            vm.modalList=true;
+                            vm.showTable=false;
+                        }
+                    }
+            })
+            vm.showTable=true;
+            vm.modalList=false;
+        },
+        loadStore(){
+            let vm=this;
+            Util.post("admin/Store/GetAllStore",{},vm,function (res,data) {
+                if(res==='1'){
+                    if(data.totalCount>0){
+                        vm.storelist = data.data;                
+                    }else{
+                        vm.storelist = [];
+                    }
+                }
+            })
+        },
+        loadDate(){
+            let  vm=this;
+            Util.post("admin/Special/Schedues/Date",{},vm,function (res,data) {
+                if(res==='1'){
+                     vm.QueryDate = data.data.SchedueDay;  
+                     vm.CurrentDate =  data.data.CurrentTime;  
+                     vm.getDataInfo();
+                }
+            })
         }
     },
     mounted () {
+        this.loadStore();
+        this.loadDate();
         this.init();
+        
     },
     watch: {
         '$route' () {
